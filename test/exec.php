872650348471ce0402abf11dfa338973778ed7ae
Vasiliy <vasilisko@gmail.com>
2018-09-26 11:14:52 +0300

    creating a class framework

--- test/exec.php
+++ test/exec.php
@@ -3,12 +3,14 @@
 include('../class/eCommerce.class');
 
 $config = array(
-  'url' => 'https://twpg-test.siab.ru/exec',
+  'url'        => 'https://twpg-test.siab.ru/exec',
   'merchantid' => '9902200976',
-  'area'    => 'development',
-  'logShow' => TRUE,
-  'logCurl' => 'full',
-  'data' => 'json'
+  'area'       => 'development',
+  'logShow'    => TRUE,
+  'logCurl'    => 'full',
+  'data'       => 'json',
+  'curl_Ssl'   => TRUE,
+  'dirCert'    => $_SERVER['DOCUMENT_ROOT'] . '/test/certs/'
 );
 
 $exec = new eCommerce($config);
@@ -21,7 +23,7 @@ if ($_POST['action']) {
     default:
       $temp['status'] = 'error';
       $temp['log'] = 'no info about action';
-      $result = json_encode($temp,true);
+      $result = json_encode($temp, TRUE);
   }
 }
 
--- test/js/script.js
+++ test/js/script.js
@@ -9,14 +9,24 @@ $(document).ready(function() {
       data: $(form_id).serialize(),
       success: function(json) {
         var status = '<div class="status">Status: ' + json.status + '</div>',
-            log    = '<div class="log">' + json.log + '</div>',
-            data   = '<div class="data">' + json.data + '</div>';
+            log    = '',
+            data   = '';
+
+        if ('data' in json && json.data) {
+          data = '<div class="data">' + json.data + '</div>';
+        }
+        if ('log' in json && json.log) {
+          log = '<div class="log">' + json.log + '</div>';
+        }
         $('.show_result').html(status + data + log);
       },
       beforeSend: function() {
         $('.show_result').html('sending...');
       },
-      error: function(data) {
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorThrown);
         $('.show_result').html('error');
       }
     });
