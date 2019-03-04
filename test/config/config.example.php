26250d45f0bace08432e8716034a588bd4be311a
Vasiliy Ivanov <vvi@siab.ru>
2019-03-04 14:14:08 +0300

    Исправление работы с сертификатом

--- test/config/config.example.php
+++ test/config/config.example.php
@@ -8,7 +8,7 @@ $config = array(
   'data'         => 'json',               // json / text формат возврата данных. По умолчанию: json
   'curl_referer' => 'https://site.name',  // Страница, откуда идет запрос
   'curl_Ssl'     => TRUE,                 // TRUE / FALSE Использовать сертификат при отправке запроса
-  'dirCert'      => '/certs/',            // Директория, где лежат сертификаты
+  'dirCert'      => '/certs/example.crt', // Ссылка на сертификат
   'language'     => 'RU'
 );
 
